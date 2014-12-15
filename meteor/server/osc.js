8e3adf6d3f7f4f5e47d4e8f69b7571b2c678fde3
Michael Garrido <michael.a.garrido@gmail.com>
2014-12-14 16:44:44 -0800

    Bosc server for TUIO multitouch events

--- meteor/server/osc.js
+++ meteor/server/osc.js
@@ -5,3 +5,34 @@ oscServer.on("message", function (msg, rinfo) {
     console.log("Message:");
     console.log(msg);
 });
+
+function parseMessage(msg) {
+
+  var data = {
+    "messageType": msg[0],
+    "timestamp": msg[1]
+    "summary": msg[2],
+    "points": []
+  };
+
+  for (var i=3,i<msg.length;i++) {
+    points.push(parsePoint(msg[i]));
+  }
+
+  return data;
+}
+
+function parsePoint(msg) {
+
+  return {
+    id: msg[3], x: msg[4], y: msg[5]
+  };
+}
+
+// check for unique points
+
+// create point objects for new points
+
+// update position for exisiting points
+
+// destroy object for missing point
