6c69fb6eb837f693f1b6179765e96a4026b7d4a4
Michael Garrido <michael.a.garrido@gmail.com>
2014-12-14 18:52:40 -0800

    adding multicast address

--- meteor/server/osc.js
+++ meteor/server/osc.js
@@ -1,21 +1,32 @@
 var osc = Meteor.npmRequire('node-osc');
+var host = '10.0.1.23';
+var multicast = '224.0.0.251';
+//var host = '0.0.0.0';
+var port = 3333;
 
-var oscServer = new osc.Server(3333, '0.0.0.0');
+var oscServer = new osc.Server(port, host, multicast);
 oscServer.on("message", function (msg, rinfo) {
     console.log("Message:");
     console.log(msg);
 });
 
+oscServer.on("listening", function () {
+  var address = server.address();
+  console.log("server listening " +
+      address.address + ":" + address.port);
+});
+
+
 function parseMessage(msg) {
 
   var data = {
     "messageType": msg[0],
-    "timestamp": msg[1]
+    "timestamp": msg[1],
     "summary": msg[2],
     "points": []
   };
 
-  for (var i=3,i<msg.length;i++) {
+  for (var i=3;i<msg.length;i++) {
     points.push(parsePoint(msg[i]));
   }
 
